d7864853e56544ef7dfb5d6b5600528dc63667e6
chematelegram <chfondia@gmail.com>
2024-02-05 01:53:53 +0100

    xigua: Initial device tree conversion

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2023 The LineageOS Project
+# Copyright (C) 2024 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_salami.mk
+    $(LOCAL_DIR)/lineage_xigua.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_salami-user \
-    lineage_salami-userdebug \
-    lineage_salami-eng
+    lineage_xigua-user \
+    lineage_xigua-userdebug \
+    lineage_xigua-eng
--- lineage_xigua.mk
+++ lineage_xigua.mk
@@ -9,25 +9,25 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from salami device
-$(call inherit-product, device/oneplus/salami/device.mk)
+$(call inherit-product, device/oneplus/xigua/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_salami
-PRODUCT_DEVICE := salami
+PRODUCT_NAME := lineage_xigua
+PRODUCT_DEVICE := xigua
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
-PRODUCT_MODEL := CPH2449
+PRODUCT_MODEL := PJA110
 
 PRODUCT_SYSTEM_NAME := $(PRODUCT_MODEL)
-PRODUCT_SYSTEM_DEVICE := OP594DL1
+PRODUCT_SYSTEM_DEVICE := OP5943L1
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="CPH2449-user 14 TP1A.220905.001 T.R4T3.150d51a_1a083-f47c release-keys" \
+    PRIVATE_BUILD_DESC="PJA110-user 14 UKQ1.230924.001 T.14b3ecd-159e3-159e1 release-keys" \
     TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
 
-BUILD_FINGERPRINT := OnePlus/CPH2449/OP594DL1:14/TP1A.220905.001/T.R4T3.150d51a_1a083-f47c:user/release-keys
+BUILD_FINGERPRINT := OnePlus/PJA110/OP5943L1:14/UKQ1.230924.001/T.14b3ecd-159e3-159e1:user/release-keys
